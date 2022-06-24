dc3d8a19872beb5457f2b6d50049771df553f13c
Semyon Shagiev <shagiev.02@gmail.com>
2022-06-25 02:25:54 +0300

    add CSV parsing

--- homework-2/src/main/java/user/User.java
+++ homework-2/src/main/java/user/User.java
@@ -1,17 +1,21 @@
 package user;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.*;
 
 @ToString
 @EqualsAndHashCode
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Getter
     @Setter
+    @CsvBindByName(column = "id", required = true)
     private int id;
     @Getter
     @Setter
+    @CsvBindByName(column = "name", required = true)
     private String name;
 
 }
